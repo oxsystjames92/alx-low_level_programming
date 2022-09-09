5dd701a34c3d4f50ecbda12808ba9a2653180f9c
oxsystjames92 <jamesjoash92@gmail.com>
2022-09-08 18:09:26 -0700

    size grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 #include <stdio.h>
 
 /**
+ * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like a multilingual puzzle"
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
@@ -16,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n". sizeof(f));
+	printf("Size of a char: %ld byte (s)\n", sizeof(C));
+	printf("Size of an int: %ld byte (s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte (s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte (s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte (s)\n", sizeof(f));
 	return (0);
 }
