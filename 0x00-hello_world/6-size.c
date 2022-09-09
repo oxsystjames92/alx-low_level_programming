0410ca61e56b28bc251957936774f6960fc05228
oxsystjames92 <jamesjoash92@gmail.com>
2022-09-09 03:36:06 -0700

    six grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry Point
  *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle"
- * Return: 0
+ * Description: prints the size of various data types
+ *
+ * Return: Always 0 (Success)
+ *
  */
-int main(void)
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
 
-	printf("Size of a char: %ld byte (s)\n", sizeof(c));
-	printf("Size of an int: %ld byte (s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte (s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte (s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte (s)\n", sizeof(f));
+int main(void)
+{	
+	printf("Size of a char: %ld byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %ld byte (s)\n", (int)sizeof(long int));
+	printf("Size of a long long int: %ld byte (s)\n", (int)sizeof(long long int));
+	printf("Size of a float: %ld byte (s)\n", (int)sizeof(float));
 	return (0);
 }
