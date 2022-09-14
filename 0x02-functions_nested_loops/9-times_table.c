6f187b9f530640e3814ff926be5b69c11a96590c
oxsystjames92 <jamesjoash92@gmail.com>
2022-09-13 18:05:12 -0700

    task nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchr(',');
+			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
@@ -21,8 +21,10 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
+			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
