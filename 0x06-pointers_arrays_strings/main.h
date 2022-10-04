5b2f11f9d6afb0721a2497729af59d61475aeb01
oxsystjames92 <jamesjoash92@gmail.com>
2022-10-04 12:03:14 -0700

    task zero

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,27 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-int _putchar(char c);
 /**
- * main - Entry point
- *
- * void protoypes()
+ * void prototypes()
  * int prototypes()
  * char prototypes()
- * Return: @dest
  */
-
-int main(void)
-{
-	int a;
-	int n;
-
-	return = 0
-}
-
+int _putchar(char c);
 void reverse_array(int *a, int n);
 void print_number(int n);
-void print_buffer(char *b, int size);
+void print_buffer(char *b. int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -34,3 +22,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/
+
